29648c1908a4566e998048fb1fa34072b17bbdc1
Tim Day <>
2020-01-16 12:58:50 +0100

    convert FillTheGapsPrep component to hooks

--- src/components/FillTheGapsPrep.js
+++ src/components/FillTheGapsPrep.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import AnswerTrackerFTGP from './AnswerTrackerFTGP';
@@ -23,76 +23,85 @@ import { updateCorrectAnswer } from '../actions/fillGapsPrep'
 //handleClickGap
   //changes the answerSet to that gap
 
-class FillTheGapsPrep extends React.Component {
-  constructor(props) {
-      super(props);
-      this.state = {
-        exerciseIndex: 0,
-        answerIndexFocus: 0
-      }
-  }
+const FillTheGapsPrep = (props) => {
 
-  handleClickNextEx = async () => {
-    const numExercises = await this.props.exercises.length
+  const [exerciseIndex, setExerciseIndex] = useState(0);
+  const [indexGapFocus, setIndexGapFocus] = useState(0);
+
+  const handleClickNextEx = async () => {
+    const numExercises = await props.exercises.length
     
-    if (this.state.exerciseIndex < numExercises-1) {
-      await this.setState(prevState => ({
-        exerciseIndex: prevState.exerciseIndex + 1,
-        answerIndexFocus: 0
-      }))
+    // TODO: clear given answers
+    // everything with class ftgp__sentence-gap is turned back into ____
+    
+    if (exerciseIndex < numExercises-1) {
+      await setExerciseIndex(exerciseIndex + 1)
+      await setIndexGapFocus(0)
     } else {
-      await this.setState({
-        exerciseIndex: 0,
-        answerIndexFocus: 0
-      })
+      await setExerciseIndex(0)
+      await setIndexGapFocus(0)
     }
   }
 
-  handleClickNextGapInEx = () => {
-    const nextGapIndex = this.state.answerIndexFocus + 1
-    const numAnswerSets = this.props.exercises[this.state.exerciseIndex].answerSet.length
+  /**
+   * knows which exercise user is in
+   * which gap of that exercise
+   * results in proper render of the answer set for that exercise
+   */
+  const handleClickNextGapInEx = async () => {
+    // increment indexGapFocus, causing re-render
+    console.log(`exerciseIndex: ${exerciseIndex}`)
 
-    if (nextGapIndex < numAnswerSets) {
-      this.setState(prevState => ({
-        answerIndexFocus: prevState.answerIndexFocus + 1
-      }))
+    const maxAnswerSetIndex = props.exercises[exerciseIndex].answerSet.length
+    console.log(`maxAnswerSetIndex: ${maxAnswerSetIndex}`)
+
+    const currentIndexGapFocus = indexGapFocus
+    console.log(`current indexGapFocus: ${currentIndexGapFocus}`)
+
+    if (currentIndexGapFocus < maxAnswerSetIndex) { //increment 
+      await setIndexGapFocus(indexGapFocus + 1)
+      console.log(`indexGapFocus: ${indexGapFocus}`)
       // remove highlight class from other elements
       // add highlight class to this element
     } else {
-      this.setState(({
-        answerIndexFocus: 0
-      }))
+      await setIndexGapFocus(0)
     }
+    console.log(`indexGapFocus: ${indexGapFocus}`)
+    console.log('back to start')
   }
 
-  handleClickGap = (evt) => {
+  //TODO:
+  const replaceAllAnswersWithGaps = () => {
+
+  }
+
+  //TODO:
+  const handleClickGap = (evt) => {
     console.log(evt.target)
     
     //get the index of the gap
     // just count it
       // get posn in html coll
       // count gaps before it
-    // const coll = 
-    // this.setState(prevState => ({ answerIndexFocus: }))
 
     // give the gap class highlighted,
     // remove class from other gaps
   }
 
-  handleClickAnswer = (evt, a) => {
+  const handleClickAnswer = (evt, a) => {
     
     if (a.correct) {
       console.log(a.indexInSentence)
-      console.log(this.state.answerIndexFocus)
-      console.log(this.props.exercises[this.state.exerciseIndex].answerSet.length-1)
+      console.log(indexGapFocus)
+      console.log(props.exercises[exerciseIndex].answerSet.length-1)
 
       // if all answers for exercise are correct -> redux update
 
       // move focus to next gap (IFF there is another gap)
       // TODO: slide transition in the next answer set
-      const lastGapIndex = this.props.exercises[this.state.exerciseIndex].answerSet.length-1
-      if (this.state.answerIndexFocus < lastGapIndex) {
-        this.handleClickNextGapInEx()
+      const lastGapIndex = props.exercises[exerciseIndex].answerSet.length-1
+      if (indexGapFocus < lastGapIndex) {
+        handleClickNextGapInEx()
       } else {
         // TODO: if still unfilled gaps, go to the first remaining
         // add focus
@@ -103,85 +112,79 @@ class FillTheGapsPrep extends React.Component {
       document.getElementsByClassName('ftgp__sentence-word')[a.indexInSentence].textContent = ` ${a.word} `
       
       // [] update redux tracker for correct answer
-      console.log('CLICKED ANSWER FOR GAP: ' + this.state.answerIndexFocus)
+      console.log('CLICKED ANSWER FOR GAP: ' + indexGapFocus)
       // exIndex, ansIndex
-      this.props.updateCorrectAnswer(this.state.exerciseIndex, this.state.answerIndexFocus)
-
-
+      props.updateCorrectAnswer(exerciseIndex, indexGapFocus)
 
       // can we change it in the data instead?
     } else {
-
       evt.target.className = 'ftgp__answer-btn ftgp__btn-incorrect'
 
     }
-    
-
     // if all sentence gaps are full, move focus to the arrow for the next exercise
-    
   }
 
   // TODO:
   // gaps get a special highlight, especially when active
   // filled in words get the preposition color
-  render () {
-    return (
-      <div className='content-container'>
-        {/* <p className='ftgp__title'>Fill The Gaps!</p> */}
+  const GAP_RENDER = '___'
 
-        <div className='ftgp__sentence slide-in'>
-        {
-          this.props.exercises[this.state.exerciseIndex].sentence.map((w,i) => {
-            let wordClasses = 'ftgp__sentence-word'
-            // let clickFcn
-            if (w === '___') { //TODO: gap may change. put it in a global constants file at some point
-              console.log('gap here, add to className')
-              wordClasses += ' ftgp__sentence-gap'
-            }
-            return (
-            <span
-              key={`key${w}${i}`}
-              className={wordClasses}
-              onClick={(w === '___') ? this.handleClickGap : ()=>console.log('gap click')} //TODO: ask if ok on SO
-            >
-              {/[.',]/.test(w[0]) ? `${w}` : ` ${w}`}
-              {/* {`${w} `} */}
-            </span>
-          )})
-        }
-        </div>
+  return (
+    <div className='content-container'>
+      {/* <p className='ftgp__title'>Fill The Gaps!</p> */}
 
-        <div className='ftgp__answer-set'>
-          {
-            this.props.exercises[this.state.exerciseIndex].answerSet[this.state.answerIndexFocus].map((a,i) => (
-              <button
-                key={`key${a.word}${i}`}
-                className='ftgp__answer-btn'
-                onClick={(evt) => this.handleClickAnswer(evt, a)}
-                onMouseDown={(evt) => evt.preventDefault()}
-              >{`${a.word}`}</button>
-            ))
+      <div className='ftgp__sentence slide-in'>
+      {
+        props.exercises[exerciseIndex].sentence.map((w,i) => {
+          let wordClasses = 'ftgp__sentence-word'
+          // let clickFcn
+          if (w === GAP_RENDER) { //TODO: gap may change. put it in a global constants file at some point
+            console.log('gap here, add to className')
+            wordClasses += ' ftgp__sentence-gap'
           }
-          <button
-            className='ftgp__answer-btn'
-            onClick={this.handleClickNextGapInEx}
-          >&#10095;
-          </button>
-        </div>
-
-        <button
-          onClick={this.handleClickNextEx}
-        >Nxt exercise
-        </button>
-
-        <div>
-          <Link to="/words-go-in">back</Link>
-        </div>
-
-        < AnswerTrackerFTGP />
+          return (
+          <span
+            key={`key${w}${i}`}
+            className={wordClasses}
+            onClick={(w === GAP_RENDER) ? handleClickGap : ()=>console.log('gap click')} //TODO: ask if ok on SO
+          >
+            {/[.',]/.test(w[0]) ? `${w}` : ` ${w}`}
+            {/* {`${w} `} */}
+          </span>
+        )})
+      }
       </div>
-    )
-  }
+
+      <div className='ftgp__answer-set'>
+        {
+          props.exercises[exerciseIndex].answerSet[indexGapFocus].map((a,i) => (
+            <button
+              key={`key${a.word}${i}`}
+              className='ftgp__answer-btn'
+              onClick={(evt) => handleClickAnswer(evt, a)}
+              onMouseDown={(evt) => evt.preventDefault()}
+            >{`${a.word}`}</button>
+          ))
+        }
+        <button
+          className='ftgp__answer-btn'
+          onClick={handleClickNextGapInEx}
+        >&#10095;
+        </button>
+      </div>
+
+      <button
+        onClick={handleClickNextEx}
+      >Nxt exercise
+      </button>
+
+      <div>
+        <Link to="/words-go-in">back</Link>
+      </div>
+
+      < AnswerTrackerFTGP />
+    </div>
+  )
 }
 
 //each exercise has sentence and answerSet (arr of arrs for each prep in sentence)
