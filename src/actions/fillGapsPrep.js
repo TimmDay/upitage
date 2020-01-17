782e776f3f5d4f1a28fb30a4fe407f49219f52bf
Tim Day <>
2020-01-17 17:54:35 +0100

    clean up fillGapsPrep actions

--- src/actions/fillGapsPrep.js
+++ src/actions/fillGapsPrep.js
@@ -13,77 +13,63 @@ export const startGenFGP = () => {
     
     // go through each sentence
     tagsBySent.forEach((sent, i) => {
-      if (sent.length > 33) { // restriction: exercises only for shorter sentences, < 33 words
-      } else {
-        let sentenceArr = [] //will contains strings for words, with GAP_BLANK for preps
-        let answerSets = [] // will contain arr of [{ value: 'word', correct: bool }], in order of prep appearance in sentence
-        let atLeastOnePrep = false
+      //only make exercises only for sentences < 33 words
+      if (sent.length > 33) return
+      
+      let sentenceArr = [] // strings for words, GAP_BLANK for preps
+      let answerSets = [] // objs like [{ value: 'word', correct: bool }], in order of prep appearance in sentence
+      let atLeastOnePrep = false
 
-        // go through each tag of this sent
-        sent.forEach((tag,j) => {
-          //IN -> preposition, TO -> 'to'
-          if (tag[0] == 'I' || tag[0] == 'T') { //check first char of each tag for preposition
-            atLeastOnePrep = true
-            sentenceArr.push(GAP_BLANK)
-            answerSets.push([{ 
-              word: wordsBySent[i][j], 
-              correct: true,
-              indexInSentence: j
-            }])
-          } else {
-            sentenceArr.push(wordsBySent[i][j]) //add the non-prep word for render
-          }
-        }) 
-
-        // restriction: at least one prep in sentence to be a viable exercise
-        if (!atLeastOnePrep) {
-          return; //abandon this sentence, go to next
+      // check each tag of this sent
+      sent.forEach((tag,j) => {
+        //IN -> preposition, TO -> 'to'
+        if (tag[0] == 'I' || tag[0] == 'T') { //check first char of each tag for preposition
+          atLeastOnePrep = true
+          sentenceArr.push(GAP_BLANK)
+          answerSets.push([{ 
+            word: wordsBySent[i][j], 
+            correct: true,
+            indexInSentence: j
+          }])
+        } else {
+          sentenceArr.push(wordsBySent[i][j]) //add the non-prep word for render
         }
+      }) 
 
-        // add confounding answers to answer set and shuffle
-        //TODO: how to ensure no match?
-        answerSets.forEach(set => {
-          const correct = set[0].word
-          let commonList = enPrepositionsCommon
-          let trickierList = enPrepositionsTrickier
+      // need at least one prep in sentence to be a viable exercise
+      if (!atLeastOnePrep) return; //abandon this sentence, go to next
 
-          // avoid dbl answers, remove correct answer from the relevant confounding list
-          if (commonList.includes(correct)) commonList.splice(commonList.indexOf(correct),1)
-          else if (trickierList.includes(correct)) trickierList.splice(trickierList.indexOf(correct),1)
-          
-          const word1 = enPrepositionsCommon[Math.floor(Math.random() * enPrepositionsCommon.length)]
-          const word2 = enPrepositionsCommon[Math.floor(Math.random() * enPrepositionsCommon.length)]
-          const word3 = enPrepositionsTrickier[Math.floor(Math.random() * enPrepositionsTrickier.length)]
+      // add confounding answers to answer set
+      answerSets.forEach(answerSet => {
+        const correct = answerSet[0].word
+        let commonList = enPrepositionsCommon
+        let trickierList = enPrepositionsTrickier
 
-          set.push({ 
-            word: word1, 
-            correct: false 
-          })
-          set.push({ 
-            word: word2, 
-            correct: false 
-          })
-          set.push({ 
-            word: word3,             
-            correct: false 
-          })
-        // shuffle the answers
-          shuffleInPlace(set) 
-        })
+        // avoid dbl answers, remove correct answer from the relevant confounding list
+        if (commonList.includes(correct)) commonList.splice(commonList.indexOf(correct),1)
+        else if (trickierList.includes(correct)) trickierList.splice(trickierList.indexOf(correct),1)
+        
+        const word1 = enPrepositionsCommon[Math.floor(Math.random() * enPrepositionsCommon.length)]
+        const word2 = enPrepositionsCommon[Math.floor(Math.random() * enPrepositionsCommon.length)]
+        const word3 = enPrepositionsTrickier[Math.floor(Math.random() * enPrepositionsTrickier.length)]
 
-        // submit to exercise arr
-        exercisesFGP.push({ 
-          sentence: sentenceArr, 
-          answerSet: answerSets,
-          trackUserAnswers: Array.from(Array(answerSets.length), () => 0)
-        })
-      }
+        answerSet.push({ word: word1, correct: false })
+        answerSet.push({ word: word2, correct: false })
+        answerSet.push({ word: word3, correct: false })
+      // shuffle the answer set
+        shuffleInPlace(answerSet) 
+      })
 
+      // submit to exercise arr
+      exercisesFGP.push({ 
+        sentence: sentenceArr, 
+        answerSet: answerSets,
+        trackUserAnswers: Array.from(Array(answerSets.length), () => 0)
+      })
     })
 
-    // shuffle the exercises arr?
+    // shuffle the exercises arr
     shuffleInPlace(exercisesFGP)
-    console.log(exercisesFGP)
 
     // store in redux
     dispatch(storeFGP(exercisesFGP))
